cc5fd09c56c418016c4a5b1e5d1cfab06c2ca184
zh6ziq <71532216+zh6ziq@users.noreply.github.com>
2020-10-15 05:25:11 +0800

    initial commit

---
+++ src/views/Country.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>This is country page.</h1>
+    <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
+      <b-dropdown-item v-for="country in countries" :key="country.alpha3code">{{
+        country.name
+      }}</b-dropdown-item>
+    </b-dropdown>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      countries: []
+    };
+  },
+
+  created() {
+    this.loadAllCountries();
+  },
+
+  methods: {
+    loadAllCountries() {
+      axios
+        .get("https://restcountries.eu/rest/v2/all")
+        .then(respond => {
+          this.countries = respond.data;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped></style>
